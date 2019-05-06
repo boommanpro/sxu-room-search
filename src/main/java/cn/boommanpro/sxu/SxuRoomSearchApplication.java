eb965f63c0e75e3d8192e1b3e8ba12d48a6a6750
wangqimeng <wangqimeng@mininglamp.com>
2019-05-06 15:23:50 +0800

    Initial commit

---
+++ src/main/java/cn/boommanpro/sxu/SxuRoomSearchApplication.java
@@ -0,0 +1,13 @@
+package cn.boommanpro.sxu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SxuRoomSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SxuRoomSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/boommanpro/sxu/SxuRoomSearchApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.boommanpro.sxu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SxuRoomSearchApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
